c4cbd6f239a04de8b43915b5a589af0c7d53893a
Matt Athayde <mathayde@uwo.ca>
2018-11-11 17:33:46 +0000

    Backend Fruit Functionality Completed.

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var express     = require('express');   // call express
 var app         = express();            // define our app using express
 var bodyParser  = require('body-parser');
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bears',{ useNewUrlParser: true }); // connect to our database
-var Bear     = require('./bear');
+mongoose.connect('mongodb://localhost:27017/fruits',{ useNewUrlParser: true }); // connect to our database
+var Fruit     = require('./fruit');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,101 +18,96 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT ||8080;    // set our port
 
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();          // get an instance of the express Router
-
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
-
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!'});
 });
 
-// more routes for our API will happen here
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
 
-    // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
-        var bear = new Bear();      // create a new instance of the Bear model
-        bear.name = req.body.name;  // set the bears name (comes from the request)
-        // save the bear and check for errors
-        bear.save(function(err) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'Bear created!' });
-        });
-    });
-
-// get all the bears (accessed at GET http://localhost:8080/api/bears)
-router.get('/bears/', function(req, res) {
-        Bear.find(function(err, bears) {
-            if (err)
-                res.send(err);
-            res.json(bears);
-        });
-    });
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
-router.route('/bears/:bear_id')
-
-    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-    .get(function(req, res) {
-        Bear.findById(req.params.bear_id, function(err, bear) {
-            if (err)
-                res.send(err);
-            res.json(bear);
-        });
-    });
+// on routes that end in /fruits
+// ----------------------------------------------------------------------------
 
 
-// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-router.put('/bears/:bear_id', function(req, res) {
-    // use our bear model to find the bear we want
-    Bear.findById(req.params.bear_id, function(err, bear) {
-
+// create a fruit (accessed at POST http://localhost:8080/api/fruits)
+router.post('/fruits', function(req, res) {
+    var fruit = new Fruit();      // create a new instance of the Fruit model
+    fruit.name = req.body.name;  // set the fruits name (comes from the request)
+    fruit.quantity = 10;  
+    fruit.price = req.body.price;  // set the fruits price (comes from the request)
+    fruit.tax = req.body.tax;  // set the fruits tax (comes from the request)
+    // save the fruit and check for errors
+    fruit.save(function(err) {
         if (err)
             res.send(err);
-
-        bear.name = req.body.name;  // update the bears info
-
-        // save the bear
-        bear.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Bear updated!' });
-        });
-
+        res.json({ message: 'Fruit created!' });
     });
 });
+// get all the fruits (accessed at GET http://localhost:8080/api/fruits)
+router.get('/fruits/', function(req, res) {
+        Fruit.find(function(err, fruits) {
+            if (err)
+                res.send(err);
+            res.json(fruits);
+        });
+    });
+    
+    
+// on routes that end in /fruits/:fruit_id
+// ----------------------------------------------------------------------------
 
-// delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
-router.delete('/bears/:bear_id', function(req, res) {
-    Bear.remove({
-        _id: req.params.bear_id
-    }, function(err, bear) {
+
+// get the fruit with that id (accessed at GET http://localhost:8080/api/fruits/:fruit_id)
+router.get('/fruits/:fruit_id', function(req, res) {
+    Fruit.findById(req.params.fruit_id, function(err, fruit) {
         if (err)
             res.send(err);
-
+        res.json(fruit);
+    });
+});
+// update the fruit with this id (accessed at PUT http://localhost:8080/api/fruits/:fruit_id)
+router.put('/fruits/:fruit_id', function(req, res) {
+    // use our fruit model to find the fruit we want
+    Fruit.findById(req.params.fruit_id, function(err, fruit) {
+        if (err)
+            res.send(err);
+        fruit.name = req.body.name;  // update the fruits info
+        fruit.quantity = req.body.quantity;  // update the fruits info
+        fruit.tax = req.body.tax;  // update the fruits info
+        fruit.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Fruit updated!' });
+        });
+    });
+});
+// delete the fruit with this id (accessed at DELETE http://localhost:8080/api/fruits/:fruit_id)
+router.delete('/fruits/:fruit_id', function(req, res) {
+    Fruit.remove({
+        _id: req.params.fruit_id
+    }, function(err, fruit) {
+        if (err)
+            res.send(err);
         res.json({ message: 'Successfully deleted' });
     });
 });
 
 
-
-// REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+// ----------------------------------------------------------------------------
 
+
+app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('We are open for business on port ' + port);
